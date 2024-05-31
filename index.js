04862e5eab886e2ded4ef4dacda75e7c00490962
HIMU005 <hashanuzzaman99@gamil.com>
2024-05-31 16:20:31 +0600

    second day commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -27,26 +28,26 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
     // jwt relate api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "3h",
       });
       res.send({ token });
     });
 
     // middlewares
     const verifyToken = (req, res, next) => {
-      // console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "unauthorized access" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
         if (error) {
-          return res.status(401).send({ message: "forbidden access" });
+          return res.status(401).send({ message: "forbidden access1" });
         }
         req.decoded = decoded;
         next();
@@ -59,7 +60,7 @@ async function run() {
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === "admin";
       if (!isAdmin) {
-        return res.status(403).send({ message: "forbidden access" });
+        return res.status(403).send({ message: "forbidden access2" });
       }
       next();
     };
@@ -74,11 +75,8 @@ async function run() {
     // get a single menu document
     app.get("/menu/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
-      console.log(query);
       const result = await menuCollection.findOne(query);
-      console.log(result);
       res.send(result);
     });
 
@@ -166,7 +164,7 @@ async function run() {
     app.get(
       "/users/admin/:email",
       verifyToken,
-      verifyAdmin,
+
       async (req, res) => {
         const email = req.params.email;
 
@@ -208,6 +206,46 @@ async function run() {
         },
       };
       const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // payment related api here
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the cart
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+    });
+
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.find().toArray();
       res.send(result);
     });
 
